a04f62a204ccdb9e7bbc713573f02e9641fc2d49
hoo42 <hoo42@uclive.ac.nz>
2019-04-09 20:46:49 +1200

    Add basis of JUnit test for the Ship class

---
+++ src/backendTesting/ShipTest.java
@@ -0,0 +1,130 @@
+package backendTesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import backend.CrewClass;
+import backend.CrewMember;
+import backend.Ship;
+import java.util.ArrayList;
+
+class ShipTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetShields() {
+		assertEquals(Ship.getShields(), 100);
+	}
+
+	@Test
+	void testGetMoney() {
+		assertEquals(Ship.getMoney(), 0);
+	}
+
+	@Test
+	void testAddMoney() {
+		assertEquals(Ship.getMoney(), 0);
+		Ship.addMoney(500);
+		assertEquals(Ship.getMoney(), 500);
+	}
+
+	@Test
+	void testDecreaseMoney() {
+		assertEquals(Ship.getMoney(), 0);
+		Ship.addMoney(500);
+		assertEquals(Ship.getMoney(), 500);
+		Ship.decreaseMoney(250);
+		assertEquals(Ship.getMoney(), 250);
+	}
+
+	@Test
+	void testGetShipCrew() {
+		CrewMember alice = new CrewMember("Alice", CrewClass.PILOT);
+		CrewMember bob = new CrewMember("Bob", CrewClass.ENGINEER);
+		ArrayList<CrewMember> crew = new ArrayList<CrewMember>();
+		crew.add(alice);
+		Ship.addCrewMember(alice);
+		assertEquals(Ship.getShipCrew(), crew);
+		crew.add(bob);
+		Ship.addCrewMember(bob);
+		assertEquals(Ship.getShipCrew(), crew);
+	}
+
+	@Test
+	void testAddCrewMember() {
+		CrewMember alice = new CrewMember("Alice", CrewClass.PILOT);
+		ArrayList<CrewMember> crew = new ArrayList<CrewMember>();
+		crew.add(alice);
+		Ship.addCrewMember(alice);
+		assertEquals(Ship.getShipCrew(), crew);
+	}
+
+	@Test
+	void testRemoveCrewMember() {
+		CrewMember alice = new CrewMember("Alice", CrewClass.PILOT);
+		CrewMember bob = new CrewMember("Bob", CrewClass.ENGINEER);
+		ArrayList<CrewMember> crew = new ArrayList<CrewMember>();
+		crew.add(alice);
+		Ship.addCrewMember(alice);
+		assertEquals(Ship.getShipCrew(), crew);
+		crew.add(bob);
+		Ship.addCrewMember(bob);
+		assertEquals(Ship.getShipCrew(), crew);
+		crew.remove(bob);
+		Ship.removeCrewMember(bob);
+		assertEquals(Ship.getShipCrew(), crew);
+	}
+
+	@Test
+	void testGetName() {
+		assertEquals(Ship.getName(), "White Whale");
+	}
+
+	@Test
+	void testSetName() {
+		assertEquals(Ship.getName(), "White Whale");
+		Ship.setName("Flying Carpet");
+		assertEquals(Ship.getName(), "Flying Carpet");
+	}
+
+	@Test
+	void testGetInventory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInInventory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddToInventory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveFromInventory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRepairShields() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDamageShields() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPilot() {
+		fail("Not yet implemented");
+	}
+
+}
