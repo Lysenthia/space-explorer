f09e7cd5139b2d197f101e5444bdf31e961e6acf
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-04-12 13:02:33 +1200

    Add javadoc to the RandomEvent classes and implement a RandomEventTypes enum to reduce the need for hard coding event types as integers

--- src/backend/RandomEventOutput.java
+++ src/backend/RandomEventOutput.java
@@ -1,9 +1,20 @@
 package backend;
 
+/**
+ * Contains a random event that occurred and the crew member involved
+ * Should only be used by the RandomEvent class
+ * @author hoo42
+ *
+ */
 public class RandomEventOutput{
 	public final int event;
 	public final CrewMember member;
 	
+	/**
+	 * Constructs a RandomEventOutput with a given event and crew member
+	 * @param event the event that occurred
+	 * @param member the crew member involved in the event
+	 */
 	public RandomEventOutput(int event, CrewMember member) {
 		this.event = event;
 		this.member = member;
