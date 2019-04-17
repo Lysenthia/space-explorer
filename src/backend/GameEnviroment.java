fe795124bceca7f38a42cd9eecfb0951ba7aa0db
rvo16 <rvo16@cs18186kq.canterbury.ac.nz>
2019-04-17 15:04:34 +1200

    Expanded game environment and added classes to uml

--- src/backend/GameEnviroment.java
+++ src/backend/GameEnviroment.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class GameEnviroment {
 
 	public static void main(String[] args) {
+		Scanner user_input = new Scanner(System.in);
 		System.out.println("Hello!");
 		System.out.println("Welcome to Space Explorers!");
-		
-		Scanner user_input = new Scanner(System.in);
+		int numDays;
+				
 		System.out.println("How many days would you like to play for? (between 3 and 10 days): ");
-		int numDays = Integer.parseInt(user_input.next());
-//		while (numDays < 3 || numDays > 10) {
-//			System.out.println("Please enter a number between 3 and 10: ");
-//			System.out.println("How many days would you like to play for? (between 3 and 10 days): ");
-//		}
+		numDays = user_input.nextInt();
+		while (numDays < 3 || numDays > 10) {
+			System.out.println("How many days would you like to play for? (between 3 and 10 days): ");
+		}
 		System.out.println("You have chosen "+numDays+" Days.");
 		
 	}
