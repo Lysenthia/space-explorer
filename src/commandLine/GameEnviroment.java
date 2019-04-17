1f68fef10a64aa1ef035d2316a98cbc105cdd9f6
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-04-17 15:40:22 +1200

    Fix problems with entering end day in command line interface

--- src/commandLine/GameEnviroment.java
+++ src/commandLine/GameEnviroment.java
@@ -3,19 +3,45 @@ import java.util.Scanner;
 import backend.*;
 
 public class GameEnviroment {
+	
+	public static boolean hasInteger(String s) {
+		Scanner reader = new Scanner(s.trim());
+		boolean hasInt = reader.hasNextInt();
+		reader.close();
+		if (hasInt) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static int extractInt(String s) {
+		Scanner reader = new Scanner(s.trim());
+		int value = reader.nextInt();
+		reader.close();
+		return value;
+	}
 
 	public static void main(String[] args) {
-		Scanner user_input = new Scanner(System.in);
+		int days = 0;
+		String line;
+		Scanner input = new Scanner(System.in);
 		System.out.println("Hello!");
-		System.out.println("Welcome to Space Explorers!");
-		int numDays;
-				
+		System.out.println("Welcome to Space Explorers!");			
 		System.out.println("How many days would you like to play for? (between 3 and 10 days): ");
-		numDays = user_input.nextInt();
-		while (numDays < 3 || numDays > 10) {
-			System.out.println("How many days would you like to play for? (between 3 and 10 days): ");
+		line = input.nextLine();
+		if (hasInteger(line)) {
+			days = extractInt(line);
 		}
-		System.out.println("You have chosen "+numDays+" Days.");
-		
+		while (days < 3 || days > 10) {
+			System.out.println("Please enter an integer between 3 and 10 inclusive: ");
+			line = input.nextLine();
+			if (hasInteger(line)) {
+				days = extractInt(line);
+			}
+		}
+		System.out.println(days);
+		input.close();
+		System.out.println("Done");
 	}
 }
