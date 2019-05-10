668b4f87dee9ff42ab5db9ead9f963ce60502831
rvo16 <rvo16@cs18241kq.canterbury.ac.nz>
2019-05-10 16:46:40 +1200

    Add repair shields screen

---
+++ src/graphicalInterface/RepairShields.java
@@ -0,0 +1,43 @@
+package graphicalInterface;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class RepairShields {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void callScreen() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RepairShields window = new RepairShields();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RepairShields() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
