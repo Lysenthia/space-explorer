1500d29d290ebcc2913e27d25010a77698f72b54
hoo42 <hoo42@uclive.ac.nz>
2019-05-16 00:41:12 +1200

    Begin implementation of save system

--- src/backendGUIExtensions/SaveSystem.java
+++ src/backendGUIExtensions/SaveSystem.java
@@ -1,5 +1,20 @@
 package backendGUIExtensions;
 
-public class SaveSystem {
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.yaml.snakeyaml.Yaml;
+import java.time.*;
+import java.util.HashMap;
 
+public class SaveSystem {
+	private static Path resourceFolder = Paths.get(System.getProperty("user.dir")+"/resources/");
+	
+	public static boolean saveGame() {
+		String name = LocalDateTime.now().toString();
+		HashMap<String, HashMap<String, HashMap<String, String>>> data = new HashMap<String, HashMap<String, HashMap<String, String>>>();
+		HashMap<String, HashMap<String, String>> planets = new HashMap<String, HashMap<String, String>>();
+		//for (Planet planet : )
+		//data.put("planets", value)
+		return false;
+	}
 }
